393079ffdb4728c6b0250fcd09b6be785d059812
DarioJmzN <a21310143@ceti.mx>
2024-03-03 16:38:15 -0600

    012

---
+++ _012_listas_remove.py
@@ -0,0 +1,7 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_012_ 
+
+Pokemon = ['geodude', 'pikachu', 'Bulbasour', 'ninetales', 'sandslash', 'haunter', 'gastly', 'dragonair', 'snorlax'] 
+#se declara la lista Pokemon en los que existen una serie de nombres de pokemon en la lista
+Pokemon.remove("pikachu") #se remueve a pikachu de la lista
+Pokemon.remove("Bulbasour") #se remueve a Bulbasour de la lista
+print (Pokemon) #se imprime la lista Pokemon
