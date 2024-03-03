956496b6cb07d2a45e4d9fbfb1df1aeb904e1c61
DarioJmzN <a21310143@ceti.mx>
2024-03-03 16:05:29 -0600

    009

---
+++ _009_listas.py
@@ -0,0 +1,14 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_009_ 
+
+# ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+# ┈┈┈╭━━━━━━━━╮┈┈┈ 
+# ┈┈┈┃╯╯╯╯╯╯╯╯┃┈┈┈ 
+# ┈┈┈┃╭┛▇┗╮▇┗╮┃┈
+# ┈┈┈┃╰┫┈╰╯┈┣╯┃┈┈┈
+# ┈┈┈╰━┃┏┳┳┓┃━╯┈┈┈
+# ┈┈┈┈┈┃╰┻┻╯┃┈┈┈┈┈
+
+LeagueOfLengens = ['lux' , 'tresh' , 'shaco' , 'garen' , 'leona'] #se hace una lista llamada LeagueOfLengens que contiene 5 datos
+
+print(LeagueOfLengens) #se imprime la lista LeagueOfLengens
+print(LeagueOfLengens[1]) #se llama al dato 1 "tresh" de la lista LeagueOfLengens
