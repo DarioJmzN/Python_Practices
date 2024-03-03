224d1adcf486ce317214021503924f5371af47f4
DarioJmzN <a21310143@ceti.mx>
2024-03-03 16:31:58 -0600

    011

---
+++ _011_listas_-del.py
@@ -0,0 +1,21 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_011_ 
+
+# ;-.          ___,
+#   `.`\_...._/`.-"`
+#     \        /      ,
+#     /()   () \    .' `-._
+#    |)  .    ()\  /   _.'
+#    \  -'-     ,; '. <
+#     ;.__     ,;|   > \
+#    / ,    / ,  |.-'.-'
+#   (_/    (_/ ,;|.<`
+#     \    ,     ;-`
+#      >   \    /
+#     (_,-'`> .'
+#         (_,'
+
+Pokemon = ['geodude', 'pikachu', 'Bulbasour', 'ninetales', 'sandslash', 'haunter', 'gastly', 'dragonair', 'snorlax'] 
+#se declara la lista Pokemon en los que existen una serie de nombres de pokemon en la lista
+del Pokemon [-2] #se elimina de la lista el dato de la posicion -2 en este caso "dragonair"
+del Pokemon [0]  #se elimina de la lista el dato de la posicion 0 en este caso "geodude"
+print (Pokemon)  #se nos muestra la lista despues de la eliminacion de los datos 
