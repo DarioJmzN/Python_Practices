4b3cb8a261cad27930d0e9e32c7d1e66cfe709d7
DarioJmzN <a21310143@ceti.mx>
2024-03-03 17:34:57 -0600

    019

---
+++ _019_tuplas_a_listas.py
@@ -0,0 +1,10 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_019_ 
+
+lista = ['rojo', 'azul', 'verde', 'amarillo'] #tupla de 4 valores
+tupla = tuple(lista) #la lista se almacena en una tupla "" la lista no se vuelve una tupla""
+print(tupla)
+
+# De la siguiente forma guardamos una tupla dentro de una lista 
+# tupla = ('rojo', 'azul', 'verde', 'amarillo')
+# lista = list(tupla) 
+# print(type(lista))
