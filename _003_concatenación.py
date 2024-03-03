7e216d925c226a8412407606a7c6a082a58e8577
DarioJmzN <a21310143@ceti.mx>
2024-03-03 11:39:34 -0600

    003

---
+++ _003_concatenación.py
@@ -0,0 +1,26 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_003_ 
+
+# ░░░░░░▄▄▄▄███▄▄▄▄░░░░░░░░░░░░░
+# ░░░▄▄█▀░░░░░░░░░▀▀▄▄░░░░░░░░░░
+# ░░█▀░░░░░░░░░░░░░░░▀█▄░░░░░░░░
+# ░█▀░░░░░░░░░░░░░░░░░░█▄░░░░░░░
+# ██░░░░░░░░░░░░░░░░░░░░█▄░░░░░░
+# █░░░░░░░░░░░░░░░░░░░░░░█▄░░░░░
+# ██░░░░░░░░░░░░▄▄▄▄▄█▀▀▀██▄░░░░
+# ▀█░░░░░░░░░▄█▀▀░░▀▀█▄░░░░█▄░░░
+# ░█▄░▄░░░░░▄█░░░░░░░░█▄░█░░█░░░
+# ░▄█▄██▄░░░█▄░░██░░░░██▄▄▄██░░░
+# ░████░▀▀░░░█▄░░░░░░▄█░░░░░██░░
+# ░█░░██▄▄░░░░▀██▄▄██▀▄▄▄▄▄▄█░░░
+# ░░▄█▀░░░░░░░░░▄▄██▀▀▀▀▀▀▀░▀█▄░
+# ░░▀█░░░░░░░▄█▀▀░░░░░░░░░░░░░█▄
+# ░░░▀█▄▄█▀░█▀░░░░░░░░░░░░░░░▄█▀
+# ░░░░░░██░▄█░░░█▀██▀▀█▀██▀▀▀▀░░
+# ░░░░░▄█░░▀█░░▀█░█░░██░██░░░░░░
+# ░░░░██▀█▄░▀█▄░▀▀████▀▀██░░░░░░
+# ░░░░█░░░▀▀█▄▀█▄▄▄▄▄▄▄▄██▄░░░░░
+
+FistWord = "A la grande le puse"  #forma1 de hacer un string guardada en una variable FistWord
+SecondWord = "Cuca" #forma1 de hacer un string guardada en una variable llamada SecondWord
+simpson = FistWord + ' ' + SecondWord #de esta forma concatenas 2 string para que cuando llamemos a print simpson nos de las 2 palabras juntas
+print (simpson) #llamas a la variable simpson
