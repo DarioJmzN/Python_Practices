98bdc3e020c0749472390425a78469cb61a39612
DarioJmzN <a21310143@ceti.mx>
2024-03-03 16:23:09 -0600

    010

---
+++ _010_posiciones negativas .py
@@ -0,0 +1,6 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_010_ 
+
+Pokemon = ['geodude', 'piachu', 'Bulbasour', 'ninetales', 'sandslash', 'haunter', 'gastly', 'dragonair', 'snorlax'] 
+#se declara la lista Pokemon en los que existen una serie de nombres de pokemon en la lista
+print(Pokemon) #se nos muestra toda la lista de pokemon
+print ("el ultimo pokemon de la lista es: " + Pokemon[-1]) #se muestra el ultimo pokemon de la lista utilizando una posicion negativa
