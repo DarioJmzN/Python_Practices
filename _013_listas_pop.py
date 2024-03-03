81b456319f626ff1885d1a1da64268ac3e6b0cf8
DarioJmzN <a21310143@ceti.mx>
2024-03-03 16:48:25 -0600

    013

---
+++ _013_listas_pop.py
@@ -0,0 +1,6 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_013_ 
+
+Pokemon = ['geodude', 'pikachu', 'Bulbasour', 'ninetales', 'sandslash', 'haunter', 'gastly', 'dragonair', 'snorlax'] #lista pokemon que guarda nombres de pokemones
+ScPokemon = Pokemon.pop(0) #se hace una nueva variable en la que se va a quitar a geodude de la lista y lo guarda en la variable
+print(Pokemon) #muestra la lista actual
+print("El pokemon eliminado de la lista y guardado es el: " + ScPokemon) #imprime un mensaje y te dice cual es el pokemon eliminado y guardado en la variable ScPokemon
