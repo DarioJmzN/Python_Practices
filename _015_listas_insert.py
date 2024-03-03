466d722bf4cb9e98ceb68fe6efab11305bf4c892
DarioJmzN <a21310143@ceti.mx>
2024-03-03 17:04:45 -0600

    015

---
+++ _015_listas_insert.py
@@ -0,0 +1,5 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_015_ 
+
+colores = ['rojo', 'azul', 'verde', 'amarillo'] #tenemos la lista colores con 4 colores diferentes
+colores.insert(0,'naranja') #llamamos a la lista colores en la cual vamos a agregar el comando .insert y vamos a poner en las condiciones que queremos agregar un nuevo apartado en la lista en la posicion que deseamos , y el nombre
+print(colores)#imprimimos en la pantalla la lista colores
