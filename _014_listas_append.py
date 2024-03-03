1141418c161f509df4aa08bcbcdd579bab167e49
DarioJmzN <a21310143@ceti.mx>
2024-03-03 16:59:05 -0600

    014

---
+++ _014_listas_append.py
@@ -0,0 +1,5 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_014_ 
+
+colores = ['rojo', 'azul', 'verde', 'amarillo'] #tenemos la lista colores con 4 colores diferentes
+colores.append('naranja') #llamamos a la lista colores y con el comando .append y le agregamos el nuevo color este se va a mandar al final de la lista
+print(colores)
