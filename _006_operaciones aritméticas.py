7a1bb17ec63f3dbaed12b06ee311d2a9304b03e2
DarioJmzN <a21310143@ceti.mx>
2024-03-03 15:42:43 -0600

    006

---
+++ _006_operaciones aritméticas.py
@@ -0,0 +1,17 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_006_ 
+
+# ░░░░░░░░░░▄▄░░░▄░░░░▄▄▄▄▄▄▄
+# ░░░░░░░░░░▀▀▀█▀▀▀█▀▀███████
+# ░░█████▄▄███████████▄▄▄▄▄▄▄▄▄▄▄▄▄
+# █▄████████████████▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+# █▄████▀░░░░██▀▀██
+# ▀░░░░░░░░░██░░░██
+# ░░░░░░░░░▀▀░░░░▀▀
+
+suma = 60 + 23 + 104 #variable suma se le esta dando una operacion de suma por la que se usa +
+resta = 212 - 30 - 112 #variable resta se le esta dando una operacion de resta por la que se usa -
+mult = 15* 23 #variable mult se le esta dando una operacion de multiplicacion por la que se usa *
+div = 6969 / 3  #variable div se le esta dando una operacion de divicion por la que se usa /
+oper = 125 / 5 + 12*4 - 63 #variable oper "operacion" en la que se esta usando la divicion(/) la suma (+) la resta (-) y multiplicacion (*)
+
+print(suma, resta, mult, div, oper) #nos muestra el print el resultado de nuestra suma, resta, mult, div y oper
