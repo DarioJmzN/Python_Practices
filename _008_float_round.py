37e52b5db2beff020fbef4aab4fbc5f56e8c29ec
DarioJmzN <a21310143@ceti.mx>
2024-03-03 15:56:27 -0600

    008

---
+++ _008_float_round.py
@@ -0,0 +1,11 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_008_ 
+
+num1 = 23.54 #declaramos la variable num1 con el un valor float 
+num2 = 45.21213131 #declaramos la variable num2 con el un valor float 
+num3 = 69.692332321312312312 #declaramos la variable num2 con el un valor float 
+print(num1 + num2 + num3) #se nos muestra la suma de los floats con todos sus decimales
+
+res = num1 + num2 + num3 #se inicia una variable que tiene como proposito almacenar la suma de todos los floats
+print(round(res,2))  #se muestra la variable res pero con una limitante de 2 decimales nada mas
+
+
