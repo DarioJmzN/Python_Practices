a810f0d0eb6a57bf47d03159bbae57aeab02109c
DarioJmzN <a21310143@ceti.mx>
2024-03-03 15:28:54 -0600

    005

---
+++ _005_saltos de línea.py
@@ -0,0 +1,13 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_005_ 
+
+# ╲┏━┳━━━━━━━━━┓╲╲
+# ╲┃◯╭┻┻╮ ╭┻┻╮┃╲╲
+# ╲┃╮┃┃╭╮┃ ┃╭╮┃┃ ┃╲╲
+# ╲┃╯┃┗┻┻┛ ┗┻┻ ┻╮╲
+# ╲┃◯┃╭╮╰╯┏━━━┳╯╲
+# ╲┃╭┃╰┏┳┳┳┳┓◯┃╲╲
+# ╲┃╰┃◯╰┗┛┗┛╯╭┃╲╲
+
+print ("Lenguajes de programacion: python \n javascript \n java \n nPHP \n TypesSc \n ".title()) #salto de linea
+print ("Lenguajes de programacion: python \n\t javascript \n\t java \n\t nPHP \n\t TypesSc".title()) #salto de linea con una tabulacion 
+
