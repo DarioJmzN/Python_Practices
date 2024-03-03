5f84b2042dadb3c44071f7bba309f62699b48bfa
DarioJmzN <a21310143@ceti.mx>
2024-03-03 17:52:32 -0600

    021

---
+++ _021_condicionales_if_else.py
@@ -0,0 +1,13 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_021_ 
+
+edad = 20 #se declara una variable entera
+if edad >= 18: #se abre una condicion (if) en la que se compara edad con 18 
+	print('persona1 de edad mayor o iguala 18 Puedes acceder, eres mayor de edad.') #se cumplio la condicion procede y hay una respuesta
+else: #no se cumple el if pero si el else
+	print('persona1 de edad menor a 18 No puedes acceder, eres menor de edad.') #no se cumplio la condicion hay otra respuesta dirente
+	
+edad1 = 15 #se declara una variable entera
+if edad1 >= 18: #se declaran las condiciones
+	print('persona2 de edad mayor o iguala 18 Puedes acceder, eres mayor de edad.') #se cumplio la condicion procede y hay una respuesta
+else: #no se cumple el if pero si el else
+	print('persona2 de edad menor a 18 No puedes acceder, eres menor de edad.') #no se cumplio la condicion hay otra respuesta dirente
