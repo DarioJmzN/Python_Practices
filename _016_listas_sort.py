778e4739ae53d4a087f9aca4aedc6daafc91abff
DarioJmzN <a21310143@ceti.mx>
2024-03-03 17:14:16 -0600

    016

---
+++ _016_listas_sort.py
@@ -0,0 +1,6 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_016_ 
+
+colores = ['rojo', 'azul', 'verde', 'amarillo']
+colores.sort(reverse=True) #llamamos a la lista colores con la funcion .sort para cambiar el acomodo y le ponemos la condicion de que sea reverse/True
+#tambien se puesde usar print(sorted(colores)) que acomodaria temporalmente la lista por orden alfavetico 
+print(colores) #imprimimos la lista colores despues del acomodo 
