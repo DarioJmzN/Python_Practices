c3f401b13eb17be6bbb2f92d96f2f8b41a6a9df5
DarioJmzN <a21310143@ceti.mx>
2024-03-03 17:28:43 -0600

    018

---
+++ _018_listas_tuplas.py
@@ -0,0 +1,9 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_018_ 
+
+lista = ['rojo', 'azul', 'verde', 'amarillo'] #las listas pueden variar, 
+#tupla = ('rojo', 'azul', 'verde', 'amarillo') #las tuplas son inmutables,lo que se define en una tupla no puede variar y proporciona seguridad al programa
+#print(tupla) #imprimimos la tupla
+print(lista) #imprimimos una lista
+
+tupla = (10,15,20, "el resultado de la operacion es: ")
+print(tupla[3], tupla[0] + tupla[2]) #imprimimos la lista, luego se genera el comentario puesto y al final nos da el valor de sumar la suma de la posicion 0 y 2
