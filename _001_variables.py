f3f1c1838fb2d1dc16aa3a06a6f87de402aff895
DarioJmzN <a21310143@ceti.mx>
2024-03-03 01:59:06 -0600

    my fist commit

--- _001_variables.py
+++ _001_variables.py
@@ -1,3 +1,5 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_001_ 
+
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 # ░░░▄▄▄░░░░▄███▄▄███▄░░░░░░▄░░▄░░░░░░░░
 # ░░░░█░░░░░██████████░░░░░░█░░█░░░░░░░░
@@ -18,9 +20,9 @@
 # ░░░░░░░░░░░░▀▀▀▀▀▄▄█▄▄▀▀░░░░░░░░░░░░░░
 # ░░░░░░░░░░░░░░░░░░░▀░░░░░░░░░░░░░░░░░░
 
-mensaje = "Este es un mensaje cualquiera."
-numero1 = 10
-numero2 = 15
-resultado = numero1 + numero2
-print (resultado)
-print (mensaje)
+mensaje = "Profe porfa paseme.... att: gracias"   #se hace una varieble de nombre "mensaje" que tiene un string con la frase Profe porfa paseme.... att: gracias ""
+numero1 = 10 #se instancia una variable llamada numero1 que da un valor entero "10"
+numero2 = 15 #se instancia una variable llamada numero2 que da un valor entero "15"
+resultado = numero1 + numero2  #se instancia una variable llamada resultado en la que se va a ser la "add" de num1 y num2 
+print (resultado) #se llama al comando print para mostrar lo que tenemos guardado en la variable resultado
+print (mensaje) #se llama al comando print para mostrar lo que tenemos guardado en la variable mensaje
