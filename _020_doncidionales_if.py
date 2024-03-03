93dca675d66187de8bb9682f1874469f40d1b85b
DarioJmzN <a21310143@ceti.mx>
2024-03-03 17:44:53 -0600

    020

---
+++ _020_doncidionales_if.py
@@ -0,0 +1,15 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_020_ 
+
+num1 = 10 #se declara una variable de valor entero
+num2 = 20 #se declara una variable de valor entero
+
+if num1 == num2: # si se comple la condicion de que num1 y num2 sean iguales nos va a mandar al siguiente paso que es una impresion que dice "Se ejecuta el if ya que num1 == num2."
+	print('Se ejecuta el if ya que num1 == num2.')
+	
+num3 = 10 #se declara una variable de valor entero
+num4 = 20 #se declara una variable de valor entero
+
+if num3 < num4: # si se comple la condicion de que num1 < num2 se imprime el mensaje
+	print('Se ejecuta el if ya que num3 < num4.')
+	
+
