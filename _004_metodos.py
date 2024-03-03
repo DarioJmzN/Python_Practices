be495b17440c859d873c660a6138457aaecbde92
DarioJmzN <a21310143@ceti.mx>
2024-03-03 15:13:35 -0600

    004

---
+++ _004_metodos.py
@@ -0,0 +1,28 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_004_ 
+
+# ░░░░░░▄█▀█▄░░░░░░░░░░░░░░░
+# ░▄█▀▀▀▀░░░░▀█▄▄▄▄▄▄▄░░░░░░
+# █▀░░░░░░░░░░░░░░░░░▀█░░░░░
+# ▀▄░▄░░░░░░░░░░░░░░░▄█░░░░░
+# ░█████▄▄▄▄▄██▄▄▄█▀▀█░░░░░░
+# ░█▀█░░░░▀░░░░░▀░░░░█▀▀▀▀▀█
+# ░█░███▄▄▄▄░░░▄▄▄▄▄██▀▀██░█
+# ░█░███░████▀████░███░░█░░█
+# ░█▄███░████░████░███░░█░█▀
+# ░░░███░████░████░███░░█░█░
+# ░░░███░████░████░███▄▄█░█░
+# ░░░███░████░████░███░░░▄█░
+# ░░░███░████░████░███▀▀▀▀░░
+# ░░▄███▄████░████▄███▄░░░░░
+# ░░███▀███████████▀███░░░░░
+# ░░░░▀▀▀██▄▄▄▄▄██▀▀▀░░░░░░░
+
+n1 = "el mejor sueño jamas asimilado por mi realidad" #haces una variable n1 que guarda un string
+n1 = n1.title() #la funcion .title nos permite cambiar las letras minusculas por mayusculas de la primera letra de cada palabra
+print(n1) #nos muestra en pantalla lo que tiene guardado "n1"
+n2 = "y si prometes no tardar tanto "
+print(n2.title()) #otra forma de hacer de usar .title
+n3 = "prometo esperate toda la vida " #upper nos va cambiar todo por mayusculas
+print(n3.upper()) 
+n4 = "NO ERES LO QUE BUSCABA ERES LO QUE NUNCA JAMAS IMAGINE TENER" #Lower va a cambiar todo a minusculas
+print(n4.lower())
