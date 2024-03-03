8dd5f45104aedc0b3b36c383c1010f0a4c9052f6
DarioJmzN <a21310143@ceti.mx>
2024-03-03 15:49:25 -0600

    007

---
+++ _007_exponentes.py
@@ -0,0 +1,10 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_007_ 
+
+#   (￣y▽￣)╭ Ohohoho.....
+#   ○( ＾皿＾)っ Hehehe…○( ＾皿＾)っ
+#   (╬▔皿▔)╯
+
+exp1 = 10 ** 2 #se declara exp1 y se le da el valor numerico de 10 elevado a la 2 y para representar al exponente se usa (**)
+exp2 = 30 ** 3 #se declara exp2 y se le da el valor numerico de 30 elevado a la 3
+exp3 = 2** 6   #se declara exp3 y se le da el valor numerico de 2 elevado a la 6
+print(exp1, exp2, exp3) #se nos muestra los valores de las varibles exp1, exp2, exp3
