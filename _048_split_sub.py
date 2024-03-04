6b6e082a25502e655682940f768af95d4cf56dc3
DarioJmzN <a21310143@ceti.mx>
2024-03-03 22:49:14 -0600

    048

---
+++ _048_split_sub.py
@@ -0,0 +1,9 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_048_
+
+import re #se importa la libreria re
+
+texto = "estoy cansado jefe aaaaaaaaaaaaaaaaaa"
+busqueda = re.split(" ", texto)
+#divide la cadena texto en subcadenas utilizando espacios en blanco como separadores y devuelve una lista con las subcadenas resultantes.
+print(busqueda) #se imprime la variable busqueda
+
