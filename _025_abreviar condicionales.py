3b3bc3c6c2a49d6782559d5b387ae528a240c713
DarioJmzN <a21310143@ceti.mx>
2024-03-03 19:13:30 -0600

    025

---
+++ _025_abreviar condicionales.py
@@ -0,0 +1,17 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_025_
+
+#variables enteras 
+x = 100
+y = 200
+c = 333
+v = 444
+d = 7774
+e = 888
+
+if x < y:  #forma 1 de hacer un if 
+    print("x es menor que y.")
+
+if c < v: print("c es menor que v.") #forma 2 de hacer un if en solo una linea 
+
+print('d es menor que e.') if d < e else print('d no es menor que e') #forma 2 de hacer un if else en solo una linea
+    
