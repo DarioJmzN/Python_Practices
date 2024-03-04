1ae953d10343e2c8e57aa5625cbfaf1b4004ed54
DarioJmzN <a21310143@ceti.mx>
2024-03-03 19:29:06 -0600

    026

---
+++ _026_While.py
@@ -0,0 +1,23 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_025_
+
+#x = 1  #se declara la variable entera x
+
+# while x < 10:
+	# print(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa que has hechoooo es un bucle infinitooooooooo aaaaaaaaaaaaaaaaaaaaaaaaaaaa)
+	
+z = 1 #se declara la variable entera z
+
+while z < 10: #mientras z sea menor de 10 se cumple la condicion 
+	print(z) #se imprime el valor de z
+	z += 1   #z incrementa en 1 y se vuelve a repetir el ciclo while hasta que no se cumpla la condicion 
+	
+
+print(      )
+
+y = 9 #se declara la variable entera y
+
+while y > 0: #mientras y sea mayor de 0 se cumple la condicion 
+	print(y)  #se imprime el valor de y
+	y -= 2    #y decrementa en 1 y se vuelve a repetir el ciclo while hasta que no se cumpla la condicion 
+	
+
