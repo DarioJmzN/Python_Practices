65b6fd251851cf951a665e442fa1d66c158c9a1d
DarioJmzN <a21310143@ceti.mx>
2024-03-03 22:34:05 -0600

    044

---
+++ _044_datetime.py
@@ -0,0 +1,7 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_044_
+
+import datetime #importa la libreria datetime 
+
+fecha = datetime.datetime.now() #utilizamos una variable llamada fecha y le asignamos a datetime.datetime.now para darle el tiempo actual
+
+print(fecha) #imprime la fecha actual
