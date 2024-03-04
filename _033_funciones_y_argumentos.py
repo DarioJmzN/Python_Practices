d9890bc0d7cc9f733b8f89794b96d757ec0f922f
DarioJmzN <a21310143@ceti.mx>
2024-03-03 20:47:15 -0600

    033

---
+++ _033_funciones_y_argumentos.py
@@ -0,0 +1,13 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_033_
+
+#hacemos una funcion llamada saludo con la palabra reservada def
+
+def familiaG(nombre):
+	print('Hola ', nombre + "  " + "Griffin")
+
+familiaG('Peter')
+familiaG('Lois')
+familiaG('Stewie')
+familiaG('Meg')
+familiaG('Brian')
+familiaG('Cris')
