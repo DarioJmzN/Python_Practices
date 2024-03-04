957af4107daf18ac65e2abb86fb892d7ca4c3e12
DarioJmzN <a21310143@ceti.mx>
2024-03-03 22:18:49 -0600

    042

---
+++ _042_variables_globales_y_locales.py
@@ -0,0 +1,9 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_042_
+
+def funcion1(): #define una funcion que no tiene parametros 
+	global num1 #se asigna una variable global llamada num1
+	num1 = 10 #se le asigna 10 a num1
+
+funcion1() #llama a la funcion1 que le asigna el valor 10 a la variable global
+
+print(num1) # se imprime la variable num1
