49feb68736c6eb38fcbbdfc9b13fb6133501cfc3
DarioJmzN <a21310143@ceti.mx>
2024-03-03 21:02:18 -0600

    035

---
+++ _035_diccionario_kwargs.py
@@ -0,0 +1,8 @@
+# Pablo Darío Jiménez Nuño 21310143 practica_035_
+
+def colores(color="rojo"): #se hace la funcion colores en el que vamos a llamar a color
+    print("mi color favorito es el: " + color) #imprimimos cada que llamamos a la funcion colores mi color favorito es el: " + el color que se asigno
+
+colores("azul") #llamamos a la funcion colores con nuestro dato azul
+colores() #llamamos a la funcion colores y como no hay dato se usa el de la funcion "rojo"
+colores("verde") #llamamos a la funcion colores con nuestro dato verde
